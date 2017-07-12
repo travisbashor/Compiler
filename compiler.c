4066e2d87192cd00596afbeb39a2b18089ca6564
Travis Bashor <travis.bashor@gmail.com>
2017-07-12 00:39:43 -0400

    fix compiler.c, mostly

--- compiler.c
+++ compiler.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "internals.h"
 #include "compiler.h"
 
+int Show_Lexemes = 0;
+int Show_Assembly = 0;
+int Show_Execution = 0;
+
 int main(int argc, char** argv) {
+  
+  // handle flags
+  int num_flags = argc - 1;
+  char** flags = argv;
+  assign_flags(num_flags, flags);
   
   printf("Please enter the name of the program you wish to compile:\n");
   char program[50];
   scanf("%s", program);
   
   compile(program);
-
+  
 }
 
 void compile(char* program) {
@@ -20,13 +30,11 @@ void compile(char* program) {
   char* parser_output = "assembly.txt";
   char* vm_output = "virtual_machine.txt";
 
-  // run the lexical analyzer
-  run_lexical_analyzer(lex_output);
+  run_lexical_analyzer(program, lex_output);
 
-  // parse
   run_parser(parser_output);
 
-  // show that the program is syntactically correct
+  // program is syntactically correct
   success();
   
   run_virtual_machine(vm_output);
@@ -35,5 +43,40 @@ void compile(char* program) {
 
 void success() {
   printf("Program is syntactically correct.\n");
+}
+
+void assign_flags(int num_flags, char** flags) {
+
+  if(num_flags > 3) {
+    flag_error(1);
+  }
+  else {
+    for(int i = 1; i <= num_flags; i++) {
+      // check for -l
+      if(strcmp(flags[i], "-l") == 0) {
+        Show_Lexemes = 1;
+      }
+      // check for -a
+      else if(strcmp(flags[i], "-a") == 0) {
+        Show_Assembly = 1;
+      }
+      // check for -v
+      else if(strcmp(flags[i], "-v") == 0) {
+        Show_Execution = 1;
+      }
+      else {
+        flag_error(2);
+      }
+    }
+  }
+}
+
+void flag_error(int num) {
+  switch(num) {
+    case 1:
+      printf("Wrong number of flags. Maximum of 3.\n");
+    case 2:
+      printf("Wrong flags. Only -l, -a, and -v are allowed.\n");
+  }
 }
 
